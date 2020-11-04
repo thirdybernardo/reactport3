e4617840a06e387e2e074a1f07def059b978e173
thirdybernardo <54013921+thirdybernardo@users.noreply.github.com>
2020-11-04 13:54:07 +0800

    cadd
    c

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import logo from './logo.svg';
 import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Route , Link } from 'react-router-dom';
+import Container from 'react-bootstrap/Container';
 
 class App extends React.Component {
 	constructor(props){
@@ -29,7 +31,11 @@ class App extends React.Component {
 	
 	render () {
       return (
-       <div> hello react </div>
+      <Router>
+	     <Container fluid={false}>
+		 <p>boostrap testing</p>
+		 </Container>
+	  </Router>
      );
 	}
 }
